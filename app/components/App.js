ef4682e95cdbe0b1ebba382120118f5d332e83fb
kulinich denis <kulinichdenis@kulinichs-MacBook-Pro.local>
2017-10-16 17:49:21 +0300

    add helpers fake data

--- app/components/App.js
+++ app/components/App.js
@@ -1,32 +1,6 @@
 import React, { Component } from 'react';
 import Table from './Table'
-
-const ITEMS = [
-  {
-    brand: 'Intel',
-    id: 10007598,
-    title: 'STK1AW32SC x5-Z8300 1.44GHz',
-    description: 'Compute Stick',
-    count: 1234,
-    price: 75.5
-  },
-  {
-    brand: 'NVidia',
-    id: 10160045,
-    title: 'Gigabyte GeForce GTX 1080 Ti Gaming',
-    description: 'Graphics Card',
-    count: 746,
-    price: 286.66
-  },
-  {
-    brand: 'Bose',
-    id: 10090403,
-    title: 'Wave SoundTouch Music System IV',
-    description: 'Wireless Speaker',
-    count: 230,
-    price: 599.95
-  }
-];
+import ITEMS from '../helpers/fakeData'
 
 class App extends Component {
   render() {
---
+++ app/helpers/fakeData.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    brand: 'Intel',
+    id: 10007598,
+    title: 'STK1AW32SC x5-Z8300 1.44GHz',
+    description: 'Compute Stick',
+    count: 1234,
+    price: 75.5
+  },
+  {
+    brand: 'NVidia',
+    id: 10160045,
+    title: 'Gigabyte GeForce GTX 1080 Ti Gaming',
+    description: 'Graphics Card',
+    count: 746,
+    price: 286.66
+  },
+  {
+    brand: 'Bose',
+    id: 10090403,
+    title: 'Wave SoundTouch Music System IV',
+    description: 'Wireless Speaker',
+    count: 230,
+    price: 599.95
+  }
+];
